c42ce4953ed937c18a285b6e7c7e5779753d16e6
haru <kawachigenji95@gmail.com>
2024-03-08 21:11:44 +0900

    Update Logger

--- src/server.php
+++ src/server.php
@@ -6,11 +6,9 @@ use Request\RequestURI;
 use Exceptions\interface\UserVisibleException;
 
 spl_autoload_extensions(".php");
-// autoloadはこのファイルを実行するプロセスの作業ディレクトリを基準にする
 spl_autoload_register(function ($class) {
     $class = str_replace("\\", "/", $class);
-    $file = $class . '.php';
-    // file_put_contents("../test/debug.txt", $file . PHP_EOL, FILE_APPEND);
+    $file = __DIR__ . '/' . $class . '.php';
     if (file_exists($file)) {
         require_once $file;
     }
